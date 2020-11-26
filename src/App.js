0cedad996654dd41ec4ab2a4fcb19d70ae57ca85
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-11-26 18:35:38 +0000

    COntent Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
 import Menu from './Menu';
@@ -9,6 +10,7 @@ export default class App extends Component {
         <Header/>
         <Menu/>
         <Footer/>
+        <Content/>
       </div>
     )
   }
